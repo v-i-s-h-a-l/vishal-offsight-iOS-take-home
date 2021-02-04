4d3ef010406d3457ea660b55fe180be4962e371e
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2021-02-04 17:55:07 +0530

    create Xcode project

---
+++ Offsight-TH/Offsight-TH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Offsight-TH
+//
+//  Created by Vishal Singh on 04/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
