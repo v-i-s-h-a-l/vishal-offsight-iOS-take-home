708d8b18ee3a453ce6457089ef5e43a61eb95996
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2021-02-04 18:41:31 +0530

    add scene delegate and a dummy view controller (this would be deleted later)

---
+++ Offsight-TH/Offsight-TH/DummyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DummyViewController.swift
+//  Offsight-TH
+//
+//  Created by Vishal Singh on 04/02/21.
+//
+
+import UIKit
+
+class DummyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+}
