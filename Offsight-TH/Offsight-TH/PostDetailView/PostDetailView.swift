6b7a3959286ee7431f0279c6d2305a1bf151d807
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2021-02-11 03:43:47 +0530

    add image on detail screen

--- Offsight-TH/Offsight-TH/PostDetailView/PostDetailView.swift
+++ Offsight-TH/Offsight-TH/PostDetailView/PostDetailView.swift
@@ -6,13 +6,35 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct PostDetailView: View {
 
     @ObservedObject var viewModel: PostDetailsViewModel
     
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Theme.Col.background
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    if let url = viewModel.imageURL {
+                        URLImage(url: url) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(.bottom)
+                                .shadow(color: Theme.Col.shadowCasted, radius: 3.0, x: 3.0, y: 3.0)
+                        }
+                    } else {
+                        Image(uiImage: UIImage(systemName: "xmark.icloud.fill")!)
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .padding(.bottom)
+                    }
+                }
+                .padding()
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
